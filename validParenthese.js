38008f2673366ecf69caccfa386dd28bc37c3083
Jun Shen <redwolf85911@gmail.com>
2018-06-12 19:35:53 +0800

    valid parenthese

---
+++ validParenthese.js
@@ -0,0 +1,33 @@
+var pairs = {
+	'(':  ')',
+	'[': ']',
+	'{': '}'
+}
+function isPair (left, right) {
+	return pairs[left] !== undefined
+}
+function isLeft (p) {
+	return Object.keys(pairs).indexOf(p) !== -1
+}
+function isRight (p) {
+	return Object.values(pairs).indexOf(p) !== -1
+}
+function validParenthese(formula)  {
+	var stack = []
+	var isValid = false
+
+	for (var i = 0; i < formula.length; i++) {
+		var curr = formula[i]
+	 	if (isLeft(curr)) {
+	 		stack.push(curr)
+	 	} else if (isRight(curr)){
+	 		if (stack.length === 0) { return false}
+	 		var prev = stack.pop()
+	 		if (!isPair(prev, curr)) {return false}
+	 	} 
+	 }
+
+	 if (stack.length > 0) {return false}
+
+	 return true
+}
