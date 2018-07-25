43559886f99f1dc87160ba9402aac557d54ff949
Aaron Shen <aaron.shen@sap.com>
2018-07-25 15:51:39 +0800

    test case rewrite

--- test/tree.test.js
+++ test/tree.test.js
@@ -1,11 +1,21 @@
-const {BST, Node, AVL, avlNode} = require('../tree.js')
+const {Node, AVLNode} = require('../tree.js')
 const {expect} = require('chai')
+
+function BSTSetup({v, l, r}) {
+  const node = new Node(v)
+  if (l) {
+    node.left = BSTSetup(l)
+  }
+  if (r) {
+    node.right = BSTSetup(r)
+  }
+  return node
+}
 
 describe('BST', () => {
   let bst
   beforeEach(() => {
-    bst = new BST()
-    bst.insert(6)
+    bst = new Node(6)
     bst.insert(4)
     bst.insert(7)
     bst.insert(5)
@@ -14,24 +24,45 @@ describe('BST', () => {
   })
 
   it('should insert node correctly', ()=> {
-    var root = new Node(6)
-    root.left = new Node(4)
-    root.left.right = new Node(5)
-    root.right = new Node(7)
-    root.right.right = new Node(9)
-    root.right.right.left = new Node(8)
-    expect(bst.root).to.deep.equal(root)
+    const model = {
+      v: 6,
+      l: {
+        v: 4,
+        r: {
+          v: 5
+        }
+      },
+      r: {
+        v: 7,
+        r: {
+          v: 9,
+          l: {
+            v: 8
+          }
+        }
+      }
+    }
+    const tree = BSTSetup(model)
+    expect(bst).to.deep.equal(tree)
   })
 
   it('should get the node of value', () => {
+    const model = {
+      v: 7,
+      r: {
+        v: 9,
+        l: {
+          v: 8
+        }
+      }
+    }
+    const tree = BSTSetup(model)
+    expect(bst.search(7)).to.deep.equal(tree)
 
-    const node7 = new Node(7)
-    node7.right = new Node(9)
-    node7.right.left = new Node(8)
-    expect(bst.search(7)).to.deep.equal(node7)
-
-    const node8 = new Node(8)
-    expect(bst.search(8)).to.deep.equal(node8)
+    const tree2 = BSTSetup({
+      v: 8
+    })
+    expect(bst.search(8)).to.deep.equal(tree2)
 
   })
 
@@ -43,42 +74,43 @@ describe('BST', () => {
   })
 
   it('should preorder traverseing: parent->left->right', ()=> {
-    const result = bst.preOrder()
+    const result = []
+    const collect = node => result.push(node)
+    bst.traversing('pre', collect)
     expect(result.map(node=>node.value)).to.deep.equal([6,4,5,7,9,8])
   })
 
   it('should inorder traversing: left->parent->right', () => {
-    const result = bst.inOrder()
+    const result = []
+    const collect = node => result.push(node)
+    bst.traversing('in', collect)
     expect(result.map(node=>node.value)).to.deep.equal([4,5,6,7,8,9])
   })
 
   it('should postorder traversing: left->right->parent', ()=> {
-    const result = bst.postOrder()
+    const result = []
+    const collect = node => result.push(node)
+    bst.traversing('post', collect)
     expect(result.map(node=>node.value)).to.deep.equal([5,4,8,9,7,6])
   })
-
-
 }) 
 
-function treeSetup({v, h, l, r}) {
-  const root = new avlNode(v)
-  root.h = h
+
+// AVL Tree
+function AVLSetup({v, h, l, r}) {
+  const node = new AVLNode(v)
+  node.h = h
   if (l) {
-    root.left = treeSetup(l)
-    root.left.parent = root
+    node.left = AVLSetup(l)
   }
   if (r) {
-    root.right = treeSetup(r)
-    root.right.parent = root
+    node.right = AVLSetup(r)
   }
-  return root
+  return node
 }
-
 describe('AVL', () => {
-
   it('should identical to initial height', ()=> {
-    let avl = new AVL()
-    avl.insert(13)
+    let avl = new AVLNode(13)
     avl.insert(10)
     avl.insert(15)
     avl.insert(5)
@@ -87,44 +119,95 @@ describe('AVL', () => {
     avl.insert(4)
     avl.insert(8)
 
-    const initialTree = {
+    const model = {
       v: 13,
-      h: -1,
+      h: 4,
       l: {
         v: 10,
-        h: -1,
+        h: 3,
         l: {
           v: 5,
-          h: 0,
+          h: 2,
           l: {
             v: 4,
-            h: 0,
+            h: 1,
           },
           r: {
             v: 8,
-            h: 0
+            h: 1
           }
         },
         r: {
           v: 11,
-          h: 0
+          h: 1
         }
       },
       r: {
         v: 15,
-        h: 1,
+        h: 2,
         r: {
           v: 16,
-          h: 0
+          h: 1
         }
       }
     }
-    const root = treeSetup(initialTree)
+    const tree = AVLSetup(model)
 
-    expect(avl.root).to.deep.equal(root)
+    expect(avl).to.deep.equal(tree)
   })
 
-  it('should automatically balanced in LL case')
+  it('should automatically balanced in LL case', () => {
+    let avl = new AVLNode()
+    avl.insert(13)
+    avl.insert(10)
+    avl.insert(15)
+    avl.insert(5)
+    avl.insert(11)
+    avl.insert(16)
+    avl.insert(4)
+    avl.insert(8)
+    avl.insert(3)
+
+    const treeBalanded = {
+      v: 13,
+      h: 4,
+      l: {
+        v: 5,
+        h: 3,
+        l: {
+          v: 4,
+          h: 2,
+          l: {
+            v: 3,
+            h: 1,
+          }
+        },
+        r: {
+          v: 10,
+          h: 2,
+          l: {
+            v: 8,
+            h: 1
+          },
+          r: {
+            v: 11,
+            h: 1
+          }
+        }
+      },
+      r: {
+        v: 15,
+        h: 2,
+        r: {
+          v: 16,
+          h: 1
+        }
+      }
+    }
+    const root = AVLSetup(treeBalanded)
+
+    expect(avl).to.deep.equal(root)
+  })
   it('should automatically balanced in RR case')
   it('should automatically balanced in LR case')
   it('should automatically balanced in RL case')
--- tree.js
+++ tree.js
@@ -4,147 +4,113 @@ class Node {
     this.left = null
     this.right = null
   }
-}
-
-class BST {
-  constructor() {
-    this.root = null
-  }
-  _insertRecur (value, node) {
-    if (value > node.value) {
-      if (node.right === null) {
-        node.right = new Node(value)
+  insert (value) {
+    if (value > this.value) {
+      if (this.right === null) {
+        this.right = new Node(value)
       } else {
-        this._insertRecur(value, node.right)
+        this.right.insert(value)
       }
     } else {
-      if (node.left === null) {
-        node.left = new Node(value)
+      if (this.left === null) {
+        this.left = new Node(value)
       } else {
-        this._insertRecur(value, node.left)
+        this.left.insert(value)
       }
-    }
-  }
-  insert(value) {
-    if (this.root === null) {
-      this.root = new Node(value)
-    } else {
-      this._insertRecur(value, this.root)
-    }
-  }
-  _searchRecur(value, node) {
-    if (node === null) {
-      return null
-    }
-
-    if (node.value === value) {
-      return node
-    } else if(value > node.value) {
-      return this._searchRecur(value, node.right)
-    } else {
-      return this._searchRecur(value, node.left)
     }
   }
   search (value) {
-    return this._searchRecur(value, this.root)
+    if (this.value === value) {
+      return this
+    } else if(this.right && value > this.value) {
+      return this.right.search(value)
+    } else if(this.left && value < this.value) {
+      return this.left.search(value)
+    }
+    return null
   }
-  _traversing(node, order) {
-    if (!node) {
-      return []
+  traversing (order, fn) {
+    if (typeof fn === 'function') {
+      switch (order) {
+      case 'pre':
+        fn(this)
+        this.left && this.left.traversing(order, fn)
+        this.right && this.right.traversing(order, fn)
+        break
+      case 'in':
+        this.left && this.left.traversing(order, fn)
+        fn(this)
+        this.right && this.right.traversing(order, fn)
+        break
+      case 'post':
+        this.left && this.left.traversing(order, fn)
+        this.right && this.right.traversing(order, fn)
+        fn(this)
+        break
+      default:
+        break
+      }
     }
-
-    const leftSubTreePath = this._traversing(node.left, order)
-    const rightSubTreePath = this._traversing(node.right, order)
-
-    switch (order) {
-    case 'pre':
-      return [node, ...leftSubTreePath, ...rightSubTreePath]
-    case 'in':
-      return [...leftSubTreePath, node, ...rightSubTreePath]
-    case 'post':
-      return [...leftSubTreePath, ...rightSubTreePath, node]
-    default:
-      return [...leftSubTreePath, node, ...rightSubTreePath]
-    }
-  }
-  preOrder(node) {
-    if (node === undefined) {
-      node = this.root
-    }
-    return this._traversing(node, 'pre')
-  }
-  inOrder(node) {
-    if (node === undefined) {
-      node = this.root
-    }
-    return this._traversing(node, 'in')
-  }
-  postOrder(node) {
-    if (node === undefined) {
-      node = this.root
-    }
-    return this._traversing(node, 'post')
   }
 }
 
-class avlNode {
+class AVLNode extends Node {
   constructor (value) {
-    this.h = 0
-    this.value = value
-    this.left = null
-    this.right = null
-    this.parent = null
+    super(value)
+    this.h = 1
   }
-  growSubTree(branch, value) {
-    const newNode = new avlNode(value)
-    this[branch] = newNode
-    newNode.parent = this
-
-    const oldH = this.h
-    const hDelta = branch === 'left' ? -1 : 1
-    this.h += hDelta
-
-    // if the new height of node is bigger than earlier
-    // then all the parent height need to be adjusted
-    if (Math.abs(this.h) > Math.abs(oldH)) {
-      let parent = this.parent
-      let sub = this
-      while(parent) {
-        parent.h += sub === parent.left ? -1 : 1
-        sub = parent
-        parent = parent.parent
-      }
-    }
-  }
-}
-class AVL extends BST {
-  _insertRecur (value, node) {
-    if (value > node.value) {
-      if (node.right === null) {
-        node.growSubTree('right', value)
+  insert (value, parent) {
+    // do insertion
+    if (value > this.value) {
+      if (this.right === null) {
+        this.right = new AVLNode(value)
       } else {
-        this._insertRecur(value, node.right)
+        this.right.insert(value, this)
       }
     } else {
-      if (node.left === null) {
-        node.growSubTree('left', value)
+      if (this.left === null) {
+        this.left = new AVLNode(value)
       } else {
-        this._insertRecur(value, node.left)
+        this.left.insert(value, this)
+      }
+    }
+    // update height
+    this.h = 1 + Math.max(this.getHeight(this.left), this.getHeight(this.right))
+
+    // rebalance
+    const bf = this.getBF()
+    if (bf < -1) {
+      if (value > this.left.value) { // LR case rotation
+        // TODO
+      } else { // LL case rotation
+        // TODO
+      }
+    } else if (bf > 1) {
+      if (value > this.right.value) { // RR case rotation
+        // TODO
+      } else { // RL case rotation
+        // TODO
       }
     }
   }
-  insert(value) {
-    if (this.root === null) {
-      this.root = new avlNode(value)
-    } else {
-      this._insertRecur(value, this.root)
-    }
+  leftRotate () {
+    const z = this
+    const y = this
+  }
+  rightRotate () {
+
+  }
+  getHeight (node) {
+    node = node || this
+    return node ? node.h : 0
+  }
+  getBF (node) {
+    node = node || this
+    return this.getHeight(node.right) - this.getHeight(node.left)
   }
 }
 
 module.exports = {
   Node,
-  BST,
-  avlNode,
-  AVL
+  AVLNode
 }
